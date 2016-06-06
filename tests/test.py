5f8d1c32bc3b686c566df9a83fbaf69a9da110a7
scienceopen <scienceopen@users.noreply.github.com>
2016-06-06 17:49:50 -0400

    test data path

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from astrometry_azel import Path
 from datetime import datetime
 from numpy import radians
 from numpy.testing import assert_allclose,run_module_suite
@@ -6,12 +7,14 @@ from numpy.testing import assert_allclose,run_module_suite
 from astrometry_azel.datetime2hourangle import datetime2sidereal
 from astrometry_azel.fits2azel import fits2azel
 
+rdir = Path(__file__).parent
+
 def test_datetime2sidereal():
     sdrtest = datetime2sidereal(datetime(2014,4,6,8), radians(-148), False)
     assert_allclose(sdrtest,2.9065780550600806,rtol=1e-5)
 
 def test_fits2azel(makeplot=['skipsolve']):
-    fitsfn = 'test/apod4.fits'
+    fitsfn = rdir/'apod4.fits'
     specTime = '2000-01-01T00:00:00'
 
     clim = None
