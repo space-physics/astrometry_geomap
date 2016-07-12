d7134bb931c46ac98d66472a1189a1dd702859ef
scienceopen <scienceopen@users.noreply.github.com>
2016-07-11 20:05:49 -0400

    cleanup

--- setup.py
+++ setup.py
@@ -3,17 +3,13 @@ from setuptools import setup
 import subprocess
 
 try:
-    subprocess.call(['conda','install','--yes','--file','requirements.txt'])
-except (Exception,KeyboardInterrupt) as e:
+    subprocess.call(['conda','install','--file','requirements.txt'])
+except Exception as e:
     pass
-
-with open('README.rst','r') as f:
-	long_description = f.read()
 
 setup(name='astrometry_azel',
       packages=['astrometry_azel'],
 	  description='Uses Astrometry.net program to generate per-pixel azimuth/elevation plate scaling',
-	  long_description=long_description,
 	  author='Michael Hirsch',
 	  url='https://github.com/scienceopen/astrometry_azel',
       install_requires=['pymap3d',
