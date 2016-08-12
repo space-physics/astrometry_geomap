6ad1caff082b93d678a71452bfe74d1b3ce53ce4
scivision <scivision@users.noreply.github.com>
2016-08-12 16:23:22 -0400

    init

---
+++ PrintSourceRaDec.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+Utility program to print source Coordinates in RA,DEC that astrometry.net found
+use this with .rdls files after solving an image
+"""
+from astropy.io import fits
+
+def sourceradec(fn):
+    with fits.open(str(fn),'readonly') as f:
+        return f[1].data
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    p = ArgumentParser()
+    p.add_argument('fn',help='.rdls file from astrometry.net')
+    p = p.parse_args()
+
+    radec = sourceradec(p.fn)
+
+    print('{} sources found in {}'.format(radec.shape[0], p.fn))
+
+    print(radec)
+
+
+
