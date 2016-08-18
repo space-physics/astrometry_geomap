45331bc474eee5ca80e5dfd18a0f45bc130665fb
Michael Hirsch <scienceopen@users.noreply.github.com>
2016-08-18 11:41:22 -0400

    Update setup_data.sh

--- setup_data.sh
+++ setup_data.sh
@@ -2,10 +2,6 @@
 # this copies the necessary star data (one-time) to your PC
 # unfortunately Astrometry.net does not currently recognize ~ or $HOME for this directory,
 # so I thought it more reliable to put it in a commonly accessible directory
-#
-# you will need to run this script as root
-# e.g.
-# sudo ./setup_data.sh
 
 mkdir -p /opt/astrometry/data
 for i in {08..19}; do
