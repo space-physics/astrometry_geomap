b6dfcd6d237e6a0b85a8c7c2feceec5b50355d07
scivision <scivision@users.noreply.github.com>
2017-02-05 02:09:45 -0500

    pip fallback

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ try:
     conda.cli.main('install','--file','requirements.txt')
 except Exception as e:
     print(e)
+    import pip
+    pip.main(['install','-r','requirements.txt'])
+    
 
 setup(name='astrometry_azel',
       packages=['astrometry_azel'],
