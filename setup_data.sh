2074e7b5faaf69974fa1a31c3847cbb6502d5807
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-04-08 21:20:31 -0400

    no-sudo

--- setup_data.sh
+++ setup_data.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # this copies the necessary star data (one-time) to your PC
-# unfortunately Astrometry.net does not currently recognize ~ or $HOME for this directory,
-# so I thought it more reliable to put it in a commonly accessible directory
+# https://www.scivision.co/setting-up-astrometry-net-program
 
-mkdir -p /opt/astrometry/data
+(
+cd ~/astrometry.net/data
+
 for i in {08..19}; do
-wget -nc -nd -P /opt/astrometry/data http://broiler.astrometry.net/~dstn/4200/index-42"$i".fits
+    wget -nc -nd http://broiler.astrometry.net/~dstn/4200/index-42"$i".fits
 done
+)
+
