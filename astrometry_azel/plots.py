f7b06b11eb92344933d9ecf9673c47e93c0019ff
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-08-20 06:29:29 -0400

    plot azel, radec in single plot each

--- astrometry_azel/plots.py
+++ astrometry_azel/plots.py
@@ -6,33 +6,46 @@ import numpy as np
 from typing import Sequence
 
 
-def plotazel(scale: xarray.Dataset, plottype: str = "contour", img: np.ndarray = None):
+def plotazel(
+    scale: xarray.Dataset, plottype: str = "singlecontour", img: np.ndarray = None
+):
 
     if "az" not in scale:
         return
 
-    fg = figure(figsize=(12, 5))
-    ax = fg.subplots(1, 2, sharey=True)
-
-    fg.suptitle(
-        f"{Path(scale.filename).name}\n({scale.lat:.2f}, {scale.lon:.2f})  {scale.time}"
-    )
-    # %%
-    axa = ax[0]
-
-    if plottype == "image":
-        hia = axa.imshow(scale["az"], origin="lower")
+    if plottype == "singlecontour":
+        fg = figure()
+        ax = fg.gca()
+        if img is not None:
+            ax.imshow(img, origin="lower", cmap="gray")
+        cs = ax.contour(scale["x"], scale["y"], scale["az"])
+        ax.clabel(cs, inline=1, fmt="%0.1f")
+        cs = ax.contour(scale["x"], scale["y"], scale["el"])
+        ax.clabel(cs, inline=True, fmt="%0.1f")
+        ax.set_xlabel("x-pixel")
+        ax.set_ylabel("y-pixel")
+        ax.set_title(
+            f"{Path(scale.filename).name}  ({scale.lat:.2f}, {scale.lon:.2f})  {scale.time}\nAzimuth / Elevation"
+        )
+        fg.set_tight_layout(True)
+        return fg
+    elif plottype == "image":
+        fg = figure(figsize=(12, 5))
+        ax = fg.subplots(1, 2, sharey=True)
+        hia = ax[0].imshow(scale["az"], origin="lower")
         hc = fg.colorbar(hia)
         hc.set_label("Azimuth [deg]")
     elif plottype == "contour":
+        fg = figure(figsize=(12, 5))
+        ax = fg.subplots(1, 2, sharey=True)
         if img is not None:
-            axa.imshow(img, origin="lower", cmap="gray")
-        cs = axa.contour(scale["x"], scale["y"], scale["az"])
-        axa.clabel(cs, inline=1, fmt="%0.1f")
+            ax[0].imshow(img, origin="lower", cmap="gray")
+        cs = ax[0].contour(scale["x"], scale["y"], scale["az"])
+        ax[0].clabel(cs, inline=1, fmt="%0.1f")
 
-    axa.set_xlabel("x-pixel")
-    axa.set_ylabel("y-pixel")
-    axa.set_title("azimuth")
+    ax[0].set_xlabel("x-pixel")
+    ax[0].set_ylabel("y-pixel")
+    ax[0].set_title("azimuth")
     # %%
     axe = ax[1]
     if plottype == "image":
@@ -43,54 +56,71 @@ def plotazel(scale: xarray.Dataset, plottype: str = "contour", img: np.ndarray =
         if img is not None:
             axe.imshow(img, origin="lower", cmap="gray")
         cs = axe.contour(scale["x"], scale["y"], scale["el"])
-        axe.clabel(cs, inline=True, fmt="%0.1f", fontsize="medium")
+        axe.clabel(cs, inline=True, fmt="%0.1f")
 
     axe.set_xlabel("x-pixel")
     axe.set_title("elevation")
+    fg.suptitle(
+        f"{Path(scale.filename).name}\n({scale.lat:.2f}, {scale.lon:.2f})  {scale.time}"
+    )
     fg.set_tight_layout(True)
 
     return fg
 
 
-def plotradec(scale: xarray.Dataset, plottype: str = "contour", img: np.ndarray = None):
+def plotradec(
+    scale: xarray.Dataset, plottype: str = "singlecontour", img: np.ndarray = None
+):
 
     if "ra" not in scale:
         return
 
-    fg = figure(figsize=(12, 5))
-    axs = fg.subplots(1, 2, sharey=True)
-
-    fg.suptitle(Path(scale.filename).name)
-
-    ax = axs[0]
-
-    if plottype == "image":
-        hri = ax.imshow(scale["ra"], origin="lower")
-        hc = fg.colorbar(hri)
-        hc.set_label("RA [deg]")
-    elif plottype == "contour":
+    if plottype == "singlecontour":
+        fg = figure()
+        ax = fg.gca()
         if img is not None:
             ax.imshow(img, origin="lower", cmap="gray")
         cs = ax.contour(scale["x"], scale["y"], scale["ra"])
         ax.clabel(cs, inline=1, fmt="%0.1f")
+        cs = ax.contour(scale["x"], scale["y"], scale["dec"])
+        ax.clabel(cs, inline=True, fmt="%0.1f")
+        ax.set_xlabel("x-pixel")
+        ax.set_ylabel("y-pixel")
+        ax.set_title(f"{Path(scale.filename).name}   Right Ascension / Declination")
+        fg.set_tight_layout(True)
+        return fg
+    elif plottype == "image":
+        fg = figure(figsize=(12, 5))
+        ax = fg.subplots(1, 2, sharey=True)
+        hri = ax[0].imshow(scale["ra"], origin="lower")
+        hc = fg.colorbar(hri)
+        hc.set_label("RA [deg]")
+    elif plottype == "contour":
+        fg = figure(figsize=(12, 5))
+        ax = fg.subplots(1, 2, sharey=True)
+        if img is not None:
+            ax[0].imshow(img, origin="lower", cmap="gray")
+        cs = ax[0].contour(scale["x"], scale["y"], scale["ra"])
+        ax[0].clabel(cs, inline=1, fmt="%0.1f")
 
-    ax.set_xlabel("x-pixel")
-    ax.set_ylabel("y-pixel")
-    ax.set_title("Right Ascension ")
+    ax[0].set_xlabel("x-pixel")
+    ax[0].set_ylabel("y-pixel")
+    ax[0].set_title("Right Ascension ")
     # %%
-    ax = axs[1]
     if plottype == "image":
-        hdi = ax.imshow(scale["dec"], origin="lower")
+        hdi = ax[1].imshow(scale["dec"], origin="lower")
         hc = fg.colorbar(hdi)
         hc.set_label("Dec [deg]")
     elif plottype == "contour":
         if img is not None:
-            ax.imshow(img, origin="lower", cmap="gray")
-        cs = ax.contour(scale["x"], scale["y"], scale["dec"])
-        ax.clabel(cs, inline=1, fmt="%0.1f")
+            ax[1].imshow(img, origin="lower", cmap="gray")
+        cs = ax[1].contour(scale["x"], scale["y"], scale["dec"])
+        ax[1].clabel(cs, inline=1, fmt="%0.1f")
 
-    ax.set_xlabel("x-pixel")
-    ax.set_title("Declination")
+    ax[1].set_xlabel("x-pixel")
+    ax[1].set_title("Declination")
+    fg.suptitle(Path(scale.filename).name)
+
     fg.set_tight_layout(True)
 
     return fg
