c3f1e1cb2dbeae69cdd31ff4f6685bf4d6a25fc1
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-06-23 11:25:29 -0400

    functionalize

--- LocateCrop.py
+++ LocateCrop.py
@@ -5,54 +5,76 @@ Handy for when you cropped visually or forgot the cropping parameters.
 
 Template matching is used.
 """
+
 import skimage.feature as skf
 import imageio
+import typing as T
 import numpy as np
 from pathlib import Path
 import argparse
 from matplotlib.pyplot import figure, show
 
-rgb2gray = [0.299, 0.587, 0.114]
+
+def load_gray(fn1: Path, fn2: Path) -> T.Tuple[np.ndarray, np.ndarray]:
+    """
+    load images in grayscale and same bit depth
+    """
+
+    rgb2gray = [0.299, 0.587, 0.114]
+
+    im1 = imageio.imread(fn1).dot(rgb2gray).astype(np.uint8)
+    im2 = imageio.imread(fn2).dot(rgb2gray).astype(np.uint8)
+
+    return im1, im2
 
 
-p = argparse.ArgumentParser()
-p.add_argument("fn1", help="original large image")
-p.add_argument("fn2", help="cropped smaller image")
-P = p.parse_args()
+def find_crop(im1: np.ndarray, im2: np.ndarray) -> T.Tuple[int, int]:
 
-fn1 = Path(P.fn1).expanduser().resolve(True)
-fn2 = Path(P.fn2).expanduser().resolve(True)
+    res = skf.match_template(im1, im2)
+    # values (-1, 1) and peak is at upper left corner of match.
+    Ul = np.unravel_index(res.argmax(), res.shape)
 
-
-im1 = imageio.imread(fn1).dot(rgb2gray).astype(np.uint8)
-im2 = imageio.imread(fn2).dot(rgb2gray).astype(np.uint8)
-
-res = skf.match_template(im1, im2)
-# values (-1, 1) and peak is at upper left corner of match.
-Ul = np.unravel_index(res.argmax(), res.shape)
-
-print(f"upper left corner of {fn2.name} in {fn1.name} is {Ul}")
+    return Ul
 
 
-overlay = np.zeros((*im1.shape, 3), dtype=im1.dtype)
-rows = slice(Ul[0], Ul[0] + im2.shape[0])
-cols = slice(Ul[1], Ul[1] + im2.shape[1])
-overlay[:, :, 0] = im1
-overlay[rows, cols, 2] = im2
+def plot_overlay(im1: np.ndarray, im2: np.ndarray, Ul: T.Sequence[int], fn1: Path, fn2: Path):
 
-# the diff may not be precisely zero everywhere if the crop did filtering
-diff = overlay[rows, cols, 0] - overlay[rows, cols, 2]
-print("sum(im1-im2) over ROI is:", diff.sum())
+    overlay = np.zeros((*im1.shape, 3), dtype=im1.dtype)
+    rows = slice(Ul[0], Ul[0] + im2.shape[0])
+    cols = slice(Ul[1], Ul[1] + im2.shape[1])
+    overlay[:, :, 0] = im1
+    overlay[rows, cols, 2] = im2
 
-fg = figure()
-axs = fg.subplots(1, 2)
-h1 = axs[0].imshow(overlay, alpha=0.6)
-axs[0].set_title(f"overlay: original {fn1.name}:red\ncrop {fn2.name}:blue")
-fg.colorbar(h1, ax=axs[0])
+    # the diff may not be precisely zero everywhere if the crop did filtering
+    diff = overlay[rows, cols, 0] - overlay[rows, cols, 2]
+    print("sum(im1-im2) over ROI is:", diff.sum())
 
-h2 = axs[1].imshow(diff)
-axs[1].set_title("im1 - im2 over ROI")
-fg.colorbar(h2, ax=axs[1])
+    fg = figure()
+    axs = fg.subplots(1, 2)
+    h1 = axs[0].imshow(overlay, alpha=0.6)
+    axs[0].set_title(f"overlay: original {fn1}:red\ncrop {fn2}:blue")
+    fg.colorbar(h1, ax=axs[0])
 
-fg.suptitle(f"upper left corner of crop on original image: {Ul}")
-show()
+    h2 = axs[1].imshow(diff)
+    axs[1].set_title("im1 - im2 over ROI")
+    fg.colorbar(h2, ax=axs[1])
+
+    fg.suptitle(f"upper left corner of crop on original image: {Ul}")
+
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument("fn1", help="original large image")
+    p.add_argument("fn2", help="cropped smaller image")
+    P = p.parse_args()
+
+    fn1 = Path(P.fn1).expanduser()
+    fn2 = Path(P.fn2).expanduser()
+
+    im1, im2 = load_gray(fn1, fn2)
+
+    Ul = find_crop(im1, im2)
+    print(f"upper left corner (pixel indices) of {fn2.name} in {fn1.name} is {Ul}")
+
+    plot_overlay(im1, im2, Ul, fn1, fn2)
+    show()
