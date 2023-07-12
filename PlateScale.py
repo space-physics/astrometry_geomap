5548187792e07b21e3baad8055b2d366fff7d890
scivision <scivision@users.noreply.github.com>
2023-07-12 14:24:10 -0400

    PlateScale.py as script

--- PlateScale.py
+++ PlateScale.py
@@ -7,7 +7,6 @@ the ".new" FITS file is the input to this program.
 """
 
 from __future__ import annotations
-import sys
 from pathlib import Path
 from datetime import datetime
 from argparse import ArgumentParser
@@ -15,11 +14,7 @@ from argparse import ArgumentParser
 import astrometry_azel.io as aio
 import astrometry_azel as ael
 
-try:
-    import astrometry_azel.plots as aep
-except ImportError as err:
-    print(f"plotting skipped: {err}", file=sys.stderr)
-    aep = None  # type: ignore
+import astrometry_azel.plots as aep
 
 
 def doplatescale(
@@ -80,56 +75,45 @@ def convert(
     P: argparse.Namespace
         command line arguments
     """
+
     scale, img = doplatescale(infn, latlon, ut1, Navg, solve, args)
 
     outfn = Path(scale.filename)
     outdir = outfn.parent
     outstem = outfn.stem
 
-    if aep is not None:
-        fnr = outdir / (outstem + "_radec.png")
-        fna = outdir / (outstem + "_azel.png")
-        print("writing", fnr, fna)
-        fg = aep.plotradec(scale, img=img)
-        fg.savefig(fnr)
-        fg = aep.plotazel(scale, img=img)
-        if fg is not None:
-            fg.savefig(fna)
-        else:
-            print("input time and coordinates to make azimuth, elevation plots", file=sys.stderr)
+    fnr = outdir / (outstem + "_radec.png")
+    fna = outdir / (outstem + "_azel.png")
+
+    print("writing", fnr, fna)
+
+    fg = aep.plotradec(scale, img=img)
+    fg.savefig(fnr)
+
+    fg = aep.plotazel(scale, img=img)
+    fg.savefig(fna)
 
     return outfn
 
 
-def main():
-    p = ArgumentParser(description="do plate scaling for image data")
-    p.add_argument("infn", help="image data file name (HDF5 or FITS)")
-    p.add_argument(
-        "-c", "--latlon", help="wgs84 coordinates of cameras (deg.)", nargs=2, type=float
-    )
-    p.add_argument(
-        "-t", "--ut1", help="override file UT1 time yyyy-mm-ddTHH:MM:SSZ or (start, stop)"
-    )
-    p.add_argument(
-        "-N",
-        "--navg",
-        help="number of frames or start,stop frames to avg",
-        nargs="+",
-        type=int,
-        default=10,
-    )
-    p.add_argument(
-        "-s", "--solve", help="run solve-field step of astrometry.net", action="store_true"
-    )
-    p.add_argument("-a", "--args", help="arguments to pass through to solve-field", default="")
-    P = p.parse_args()
+p = ArgumentParser(description="do plate scaling for image data")
+p.add_argument("infn", help="image data file name (HDF5 or FITS)")
+p.add_argument("-c", "--latlon", help="wgs84 coordinates of cameras (deg.)", nargs=2, type=float)
+p.add_argument("-t", "--ut1", help="override file UT1 time yyyy-mm-ddTHH:MM:SSZ or (start, stop)")
+p.add_argument(
+    "-N",
+    "--navg",
+    help="number of frames or start,stop frames to avg",
+    nargs="+",
+    type=int,
+    default=10,
+)
+p.add_argument("-s", "--solve", help="run solve-field step of astrometry.net", action="store_true")
+p.add_argument("-a", "--args", help="arguments to pass through to solve-field", default="")
+P = p.parse_args()
 
-    path = Path(P.infn).expanduser()
+path = Path(P.infn).expanduser()
 
-    print(P.latlon)
+print(P.latlon)
 
-    convert(path, P.ut1, P.navg, P.solve, P.latlon, P.args)
-
-
-if __name__ == "__main__":
-    main()
+convert(path, P.ut1, P.navg, P.solve, P.latlon, P.args)
